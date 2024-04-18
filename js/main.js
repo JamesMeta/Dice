d091ce7fc0b6b367c8ea6203462bedc8be8329cb
JamesMeta <jamesmata556@gmail.com>
2024-04-17 23:26:26 -0400

    updated dice screen to add modern look aswell as more modularity to each dice roll

--- js/main.js
+++ js/main.js
@@ -1,92 +1,66 @@
-function onPageLoad(){
-    var a = document.getElementById("Dice1")
-    var b = document.getElementById("Dice2")
-    var c = document.getElementById("Dice3")
-    var d = document.getElementById("Dice4")
-    var e = document.getElementById("Dice5")
+let diceList = [];
 
-    const ctxa = a.getContext("2d");
-    const ctxb = b.getContext("2d");
-    const ctxc = c.getContext("2d");
-    const ctxd = d.getContext("2d");
-    const ctxe = e.getContext("2d");
+function addNewDie() {
+    const size = parseInt(prompt("Enter the size of the die:"));
+    const amount = parseInt(prompt("Enter the number of dice to roll:"));
 
-    const door = new Image();
-    door.src = "images/0.png"
+    if (!isNaN(size) && !isNaN(amount)) {
+        diceList.push({ size, amount });
+        console.log(`Added a ${size}-sided die with ${amount} rolls.`);
+    } else {
+        console.log("Invalid input. Please enter valid numbers.");
+    }
+}
 
-    door.onload = function() {
-        ctxa.drawImage(door, 50, 100);
-        ctxb.drawImage(door, 50, 100);
-        ctxc.drawImage(door, 50, 100);
-        ctxd.drawImage(door, 50, 100);
-        ctxe.drawImage(door, 50, 100);
+async function rollDice() {
+    const canvasContainer = document.getElementById("canvas");
+    canvasContainer.innerHTML = ""; // Clear previous canvases
+
+    for (let i = 0; i < diceList.length; i++) {
+        const { size, amount } = diceList[i];
+
+        for (let j = 0; j < amount; j++) {
+            await rollSingleDie(size);
+        }
+    }
+}
+
+async function rollSingleDie(size) {
+    const canvasContainer = document.getElementById("canvas");
+    const canvas = document.createElement("canvas");
+    canvas.width = 200;
+    canvas.height = 300;
+    canvas.style.border = "1px solid";
+    canvasContainer.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+
+    for (let i = 0; i < 20; i++) {
+        await sleep(75);
+        const result = getRandomNumber(1, size);
+        drawDice(ctx, size, result);
+    }
+}
+
+
+
+
+function drawDice(ctx, size, result) {
+    const imageLocation = `images/${result}.png`;
+    const image = new Image();
+    image.src = imageLocation;
+    image.onload = function () {
+        ctx.canvas.width = ctx.canvas.clientWidth;
+        ctx.canvas.height = ctx.canvas.clientHeight;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(image, 50, 100);
     };
 }
 
-// display 10 random dice images before landing on one final random dice image
-async function rollDice(){
-    console.log("rollDice() called");
-    const imageMap = new Map();
-
-    for (let i = 1; i <= 20; i++) {
-        const imageLocation = `images/${i}.png`;
-        imageMap.set(i, imageLocation);
-    }
-
-    var a = document.getElementById("Dice1")
-    var b = document.getElementById("Dice2")
-    var c = document.getElementById("Dice3")
-    var d = document.getElementById("Dice4")
-    var e = document.getElementById("Dice5")
-
-    const ctxa = a.getContext("2d");
-    const ctxb = b.getContext("2d");
-    const ctxc = c.getContext("2d");
-    const ctxd = d.getContext("2d");
-    const ctxe = e.getContext("2d");
-
-    const dice = new Map();
-    dice.set(1, ctxa);
-    dice.set(2, ctxb);
-    dice.set(3, ctxc);
-    dice.set(4, ctxd);
-    dice.set(5, ctxe);
-
-    var size = document.getElementById("dsize").value;
-    var amount = document.getElementById("damount").value;
-    console.log(size);
-    console.log(amount);
-
-    for (let i = 1; i <= amount; i++) {
-        for (let j = 1; j <= 20; j++) { 
-            await sleep(40);
-            const randomDice = getRandomNumber(1, size);
-            const imageLocation = imageMap.get(randomDice);
-            const ctx = dice.get(i);
-            const image = new Image();
-            image.src = imageLocation;
-            image.onload = function() {
-                ctx.canvas.width = ctx.canvas.clientWidth;
-                ctx.canvas.height = ctx.canvas.clientHeight;
-                ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-                ctx.drawImage(image, 50, 100);
-            };
-        }
-        const randomDice = getRandomNumber(1, size);
-        console.log(randomDice);
-        const imageLocation = imageMap.get(randomDice);
-        console.log(imageLocation);
-        const ctx = dice.get(i);
-        const image = new Image();
-        image.src = imageLocation;
-        image.onload = function() {
-            ctx.canvas.width = ctx.canvas.clientWidth;
-            ctx.canvas.height = ctx.canvas.clientHeight;
-            ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-            ctx.drawImage(image, 50, 100);
-        };
-    }
-
+function reset() {
+    const canvasContainer = document.getElementById("canvas");
+    canvasContainer.innerHTML = "";
+    diceList = [];
 }
 
 function getRandomNumber(min, max) {
@@ -96,4 +70,3 @@ function getRandomNumber(min, max) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
